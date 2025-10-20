f8b5508c6c5eed94d3f4d2332e5952a522150ae2
Muhammad Fahim <fahimorakzai09@gmail.com>
2025-10-20 18:18:02 +0500

    commit this Please

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,7 @@ function FoodDisplay({ category }) {
             <h2 className='text-2xl font-bold my-5'>Top Dishes Near You</h2>
             <div className="flex justify-center gap-2 md:justify-between flex-wrap">
                 {
-                    food_list.map((item, index) => {
+                    food_list?.map((item, index) => {
                         if (category === "All" || category === item.category) {
                             return (<FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />)
                         }
