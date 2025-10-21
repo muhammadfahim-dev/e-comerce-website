60400a7c970a9583a4743c6e40e873959ea4faf0
Muhammad Fahim <fahimorakzai09@gmail.com>
2025-10-21 17:48:49 +0500

    new things added

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,6 +1,6 @@
-import app from "./app.js"
+import app from "../app.js"
 import dotenv from 'dotenv'
-import { connectDB } from "./db/index.db.js"
+import { connectDB } from "../db/index.db.js"
 
 dotenv.config({
     path: "./.env"
