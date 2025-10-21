8b94a622ed21a9434c61374de06948fd41edd470
Muhammad Fahim <fahimorakzai09@gmail.com>
2025-10-21 20:51:41 +0500

    change image url

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "../../index.css"
 
 function Header() {
     return (
-        <div className='h-[40vh] md:h-[34vw] w-[100%] bg-[url(./public/header_img.png)] bg-cover bg-no-repeat overflow-hidden rounded-2xl py-auto relative'>
+        <div className='h-[40vh] md:h-[34vw] w-[100%] bg-[url(/header_img.png)] bg-cover bg-no-repeat overflow-hidden rounded-2xl py-auto relative'>
             <div className="animate-[fadeIn_1.5s_ease-in-out_forwards] flex absolute flex-col gap-2 md:gap-5 items-start mx-w-[50%] bottom-5 left-5">
                 <h2 className='text-md font-medium  md:text-[4.5vw]/18 md:font-medium text-white '>Order Your <br /> Favorite Food Here</h2>
                 <p className='text-[10px] md:text-sm w-140 text-white'>choose from a diverse menu featuring delectable array of dishes crafted with the finest ingredients and culinary expertise . Our mission is to satisfy your cravings and elevate your dining experience, One delicious meal at a time.</p>
